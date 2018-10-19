7d748a0d2820bd9e3479a6f36c5fc837f3776cb2
Marcell Toth <marcell@marcelltoth.net>
2018-10-20 01:12:04 +0200

    Add fo-dicom, display image

--- WWFOC/Form1.Designer.cs
+++ WWFOC/Form1.Designer.cs
@@ -28,13 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.imageHolder = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // imageHolder
+            // 
+            this.imageHolder.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.imageHolder.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imageHolder.Location = new System.Drawing.Point(12, 43);
+            this.imageHolder.Name = "imageHolder";
+            this.imageHolder.Size = new System.Drawing.Size(705, 533);
+            this.imageHolder.TabIndex = 0;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(972, 724);
+            this.ClientSize = new System.Drawing.Size(729, 588);
+            this.Controls.Add(this.imageHolder);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -42,6 +56,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel imageHolder;
     }
 }
 
--- WWFOC/Form1.cs
+++ WWFOC/Form1.cs
@@ -1,20 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
+using Dicom;
+using Dicom.Imaging;
 
 namespace WWFOC
 {
     public partial class Form1 : Form
     {
+
+        public string SourcePath { get; set; } = "C:/Users/marce/OneDrive/Junction/Dataset/386348";
+
+        public int SourceFrame { get; set; } = 69;
+
+        public Bitmap ImageToDraw { get; set; }
+        
+        
         public Form1()
         {
             InitializeComponent();
+            Load += OnLoad;
+            imageHolder.Paint += ImageHolderOnPaint;
+        }
+
+        private void ImageHolderOnPaint(object sender, PaintEventArgs e)
+        {
+            float targetHeight = e.Graphics.VisibleClipBounds.Height;
+            float targetWidth = e.Graphics.VisibleClipBounds.Width;
+            e.Graphics.FillRectangle(new SolidBrush(Color.Black), new RectangleF(0, 0, targetWidth, targetHeight));
+            
+            if (ImageToDraw != null)
+            {
+                // resize if needed
+                if (!(Math.Abs(ImageToDraw.Height - targetHeight) < 2) &&
+                    !(Math.Abs(ImageToDraw.Width - targetWidth) < 2))
+                {
+                    float scale = Math.Min(targetWidth / ImageToDraw.Width, targetHeight / ImageToDraw.Height);
+                    var scaleWidth = (int)(ImageToDraw.Width * scale);
+                    var scaleHeight = (int)(ImageToDraw.Height * scale);
+                    e.Graphics.DrawImage(ImageToDraw, ((int)targetWidth - scaleWidth)/2, ((int)targetHeight - scaleHeight)/2, scaleWidth, scaleHeight);
+                }
+                else
+                {
+                    e.Graphics.DrawImage(ImageToDraw, 0, 0);
+                }
+                
+            }
+        }
+
+        private async void OnLoad(object sender, EventArgs e)
+        {
+            DicomFile sourceFile = await DicomFile.OpenAsync(Path.Combine(SourcePath, "MR.386348.Image 96.dcm"));
+            ImageToDraw = new DicomImage(sourceFile.Dataset).RenderImage().AsClonedBitmap();
         }
     }
 }

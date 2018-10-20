62b03a7a5a44028038a60503b29b849c23b6a8aa
Marcell Toth <marcell@marcelltoth.net>
2018-10-20 17:56:33 +0200

    Final image draws on the original, not the contour map

--- WWFOC/ImageProcessor.cs
+++ WWFOC/ImageProcessor.cs
@@ -58,12 +58,11 @@ namespace WWFOC
             ci.BackwardFourierTransform();
             image = ci.ToBitmap();*/
             
-            image = new Median(5).Apply(image);
-            result.Add(new ImageOutput(image, "Median"));
+            var median = new Median(5).Apply(image);
+            result.Add(new ImageOutput(median, "Median"));
             
-            var inputArray = new Image<Gray, byte>(image);
-            Mat hierarchy = new Mat();
-            var dil = inputArray.Dilate(2);
+            var medianCv = new Image<Gray, byte>(median);
+            var dil = medianCv.Dilate(2);
             Bitmap dilBitMap = dil.Bitmap.Clone(new Rectangle(Point.Empty, dil.Bitmap.Size), PixelFormat.Format32bppRgb);
             result.Add(new ImageOutput(dilBitMap, "Dilated"));
 
@@ -74,15 +73,26 @@ namespace WWFOC
             var cannyCv = colorFilteredCv.Canny(Parameter1, Parameter2);
             result.Add(new ImageOutput(cannyCv.ToBitmap(), "Contours"));
             
+            Bitmap bm = DrawFinal(cannyCv, colorFilteredCv, image);
+
+            result.Add(new ImageOutput(bm, "Final"));
+            return result;
+        }
+
+        private static Bitmap DrawFinal(Image<Gray, byte> cannyCv, Image<Gray, byte> colorFilteredCv, Bitmap background)
+        {
             VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+            Mat hierarchy = new Mat();
             CvInvoke.FindContours(cannyCv, contours, hierarchy, RetrType.Ccomp, ChainApproxMethod.ChainApproxSimple);
-                        
-            Bitmap bm = new Bitmap(colorFiltered.Width, colorFiltered.Height);
+            
+            if (contours.Size >= 250) 
+                return background;
+
+            Bitmap bm = new Bitmap(colorFilteredCv.Width, colorFilteredCv.Height);
             using (Graphics g = Graphics.FromImage(bm))
             {
-                g.DrawImage(dil.ToBitmap(), Point.Empty);
-                Pen p = new Pen(Color.Red);
-                p.Width = 2;
+                g.DrawImage(background, Point.Empty);
+                Pen p = new Pen(Color.Red) {Width = 2};
                 for (int i = 0; i < contours.Size; i++)
                 {
                     VectorOfPoint contour = contours[i];
@@ -101,16 +111,15 @@ namespace WWFOC
                                     Point p2 = contour[i2];
                                     g.DrawLine(p, p1, p2);
                                 }
-                                g.DrawLine(p, contour[0], contour[contour.Size-1]);
+
+                                g.DrawLine(p, contour[0], contour[contour.Size - 1]);
                             }
                         }
                     }
-                    
                 }
             }
-            
-            result.Add(new ImageOutput(bm, "Final"));
-            return result;
+
+            return bm;
         }
 
         private Bitmap FilterRange(Bitmap image)
